02bc48df225c8477e14ef907a88c474e61ca51b6
Dext4er0016 <nicschmid@hispeed.ch>
2018-05-31 09:19:19 +0200

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Button, ButtonToolbar, Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ButtonToolbar>
-          <Button>Default</Button>
-        </ButtonToolbar>;
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">Room Manager</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavDropdown eventKey={3} title="Reservation" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Reservation anzeigen</MenuItem>
+              </NavDropdown>
+              <NavDropdown eventKey={3} title="Räume" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Raum suchen</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>;
       </div>
     );
   }
