3f946ca47d5f2c58e5d6789ce317a7d35deb3653
Kay <kay.kunze@swisslife.ch>
2018-05-31 10:36:22 +0200

    reservationDetail

---
+++ src/components/ReservationDetail.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class ReservationDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test : ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Reservation</h1>
+      </div>
+    );
+  }
+}
+
+export { Reservation };
